98b1181fbc9d057c9001e608b0ee97ff8d61a065
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-11-29 13:05:04 +0900

    Chore: 자주쓰는 path 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@static': path.resolve(__dirname, 'static'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@layouts': path.resolve(__dirname, 'src/layouts'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@styles': path.resolve(__dirname, 'src/styles')
+    }
+  }
 })
