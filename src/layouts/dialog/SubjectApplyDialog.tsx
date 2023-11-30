67e0863802e6d4c81198987e1f7a145b3d75fb7e
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-11-30 22:51:42 +0900

    Feat: 시뮬레이션 API 연결

--- src/layouts/dialog/SubjectApplyDialog.tsx
+++ src/layouts/dialog/SubjectApplyDialog.tsx
@@ -1,15 +1,15 @@
 import DialogTemplate from '../DialogTemplate.tsx';
+import {ApplyType, ISubject} from '../../constant/types.ts';
 import '@styles/dialog/MacroDialog.scss';
-import {ApplyType} from '../../constant/types.ts';
 
 interface IMacroDialog {
   isOpen: boolean;
   closeDialog: () => void;
   nextStep: (_?:ApplyType) => void;
-  subjectName: string;
+  selectedSubject: ISubject|null;
 }
 
-function SubjectApplyDialog({isOpen, closeDialog, nextStep, subjectName}: IMacroDialog) {
+function SubjectApplyDialog({isOpen, closeDialog, nextStep, selectedSubject}: IMacroDialog) {
   function submit() {
     nextStep(ApplyType.SUCCESS);
   }
@@ -26,7 +26,7 @@ function SubjectApplyDialog({isOpen, closeDialog, nextStep, subjectName}: IMacro
         <div>
           <img src='/sample.png' alt=''/>
           <span>선택한 과목을 수강신청하시겠습니까?</span>
-          <span>교과목명(Course Title): {subjectName}</span>
+          <span>교과목명(Course Title): {selectedSubject ? selectedSubject.courseTitle : '<???>'}</span>
         </div>
       </div>
       <div className='dialog_footer'>
--- src/pages/SimulationPage.tsx
+++ src/pages/SimulationPage.tsx
@@ -1,22 +1,59 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
+import {useNavigate} from 'react-router-dom';
 import PageDefaultLayout from '../layouts/PageDefaultLayout.tsx';
 import MacroDialog from '../layouts/dialog/MacroDialog.tsx';
 import SubjectApplyDialog from '../layouts/dialog/SubjectApplyDialog.tsx';
 import ApplySuccessDialog from '../layouts/dialog/ApplySuccessDialog.tsx';
 import ApplyFailDialog from '../layouts/dialog/ApplyFailDialog.tsx';
 import ApplyDoneDialog from '../layouts/dialog/ApplyDoneDialog.tsx';
-import {ApplyType, DataFormats} from '../constant/types.ts';
+import {ApplyType, DataFormats, ISubject} from '../constant/types.ts';
+import {IErrorTypes} from '../constant/CheckFetchError.ts';
+import API from '../constant/API.ts';
 import '@styles/components/TableStyle.scss';
 
 function SimulationPage() {
+  const navigate = useNavigate();
+  
   const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
   const [applyType, setApplyType] = useState<ApplyType>(ApplyType.Macro);
-  const [selectedSubjectTitle, setSelectedSubjectTitle] = useState<string>('');
+  const [selectedSubject, setSelectedSubject] = useState<ISubject|null>(null);
+
+  const [onSimulation, setOnSimulation] = useState<boolean>(false);
+  const [simulationId, setSimulationId] = useState<number>(-1);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [subjects, setSubjects] = useState<ISubject[]>([]);
+  const [appliedSubjects, setAppliedSubjects] = useState<ISubject[]>([]);
 
   document.title = 'AllCll | 수강신청';
 
-  function openDialog(subjectTitle: string) {
-    setSelectedSubjectTitle(subjectTitle);
+  // 이미 시뮬레이션 진행 중인지 확인
+  useEffect(() => {
+    const playId = localStorage.getItem('playId');
+    if (playId === null) return;
+
+    const Errors: IErrorTypes[] = [
+      {errorBody: 'Mock not found',
+        errorMessage: '시뮬레이션을 찾을 수 없습니다.',
+        action: () => {
+          setOnSimulation(false);
+          localStorage.removeItem('playId');
+        }
+      },
+    ]
+
+    setLoading(true);
+    API.fetch2Json('/api/v2/mock/status', 'GET', {param: playId}, Errors, navigate)
+      .then((res) => {
+        setSimulationId(Number(playId));
+        setSubjects(res.interestedCourseToRegister.courses);
+        setOnSimulation(true);
+      })
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, [navigate]);
+
+  function openDialog(subject: ISubject) {
+    setSelectedSubject(subject);
     setApplyType(ApplyType.Macro);
     setIsDialogOpen(true);
   }
@@ -30,15 +67,50 @@ function SimulationPage() {
     else if (applyType < ApplyType.SUCCESS)
       setApplyType(prev => prev+1);
     else {
-      // Todo: Refresh table
+      setAppliedSubjects(prev => [...prev, selectedSubject!]);
+      refreshTable();
       closeDialog();
     }
+  }
+
+  function startSimulation() {
+    setLoading(true);
+    API.fetch2Json('/api/v2/mock/start', 'GET', {}, [], navigate)
+      .then((res) => {
+        console.log(res);
+
+        localStorage.setItem('playId', res.interestedCourse.playId);
+        setSimulationId(res.interestedCourse.playId);
+        setSubjects(res.interestedCourse.courses);
+        setOnSimulation(true);
+      })
+      .catch((err) => {
+        alert('시뮬레이션을 시작할 수 없습니다');
+        console.error(err);
+      })
+      .finally(() => setLoading(false));
+  }
+
+  function refreshTable() {
+    setLoading(true);
+    API.fetch2Json('/api/v2/mock/courses/unregistered', 'GET', {param: simulationId}, [], navigate)
+      .then((res) => {
+        console.log(res);
+
+        setSubjects(res.unregisteredInterestedCourses);
+        setOnSimulation(true);
+      })
+      .catch((err) => {
+        alert('적절하지 않은 접근입니다');
+        console.error(err);
+      })
+      .finally(() => setLoading(false));
   }
 
   return (
     <>
       <MacroDialog isOpen={isDialogOpen && applyType === ApplyType.Macro} closeDialog={closeDialog} nextStep={nextStep}/>
-      <SubjectApplyDialog isOpen={isDialogOpen && applyType === ApplyType.Apply} closeDialog={closeDialog} nextStep={nextStep} subjectName={selectedSubjectTitle}/>
+      <SubjectApplyDialog isOpen={isDialogOpen && applyType === ApplyType.Apply} closeDialog={closeDialog} nextStep={nextStep} selectedSubject={selectedSubject}/>
       <ApplySuccessDialog isOpen={isDialogOpen && applyType === ApplyType.SUCCESS} closeDialog={closeDialog} nextStep={nextStep}/>
       <ApplyFailDialog isOpen={isDialogOpen && applyType === ApplyType.FAIL} closeDialog={closeDialog}/>
       <ApplyDoneDialog isOpen={isDialogOpen && applyType === ApplyType.DONE} closeDialog={closeDialog}/>
@@ -51,32 +123,56 @@ function SimulationPage() {
             </select>
             <input placeholder='학수번호를 입력하세요' type='text' disabled/>
           </div>
-          <button onClick={() => openDialog('테스트 과목')}>
+          <button onClick={startSimulation} disabled={onSimulation || loading}>
             <img src='/Search.svg' alt=''/>
             시작
           </button>
         </div>
 
-        <table className='container_box'>
-          <thead>
-          <tr>
-            <th></th>
-            {Object.values(DataFormats.SubjectTitles).map((title, index) => (
-              <th key={index}>{title}</th>
+        {!onSimulation ? (
+          <div className='container_box'>시작 버튼을 눌러 수강신청을 시작하세요</div>
+        ) : loading ? (
+          <div className='container_box'>로딩 중...</div>
+        ) : !subjects.length ? (
+          <div className='container_box'>관심과목이 존재하지 않습니다</div>
+        ) : (
+          <table className='container_box'>
+            <thead>
+            <tr>
+              <th></th>
+              {Object.values(DataFormats.SubjectTitles).map((title, index) => (
+                <th key={index}>{title}</th>
+              ))}
+            </tr>
+            </thead>
+            <tbody>
+            {subjects.map((subject, index) => (
+              <tr key={index}>
+                <td>
+                  <button onClick={() => openDialog(subject)}>신청</button>
+                </td>
+                {Object.values(DataFormats.SubjectTitles).map((title, index) => (
+                  <td key={index}>{title}</td>
+                ))}
+              </tr>
             ))}
-          </tr>
-          </thead>
-          <tbody>
-          <tr>
-            <td>
-              <button>신청</button>
-            </td>
-            {Object.values(DataFormats.SubjectTitles).map((title, index) => (
-              <td key={index}>{title}</td>
+            </tbody>
+          </table>
+        )}
+
+        <div className='title_container_box'>
+          <div className='container_header_layout'>
+            <h2>신청완료 된 과목</h2>
+            <button className='small' onClick={refreshTable} disabled={!onSimulation || loading}>재조회</button>
+          </div>
+
+          <div className='container_box grid_layout'>
+            {appliedSubjects.map((subject, index) => (
+              <div key={index}>{subject.courseTitle}</div>
             ))}
-          </tr>
-          </tbody>
-        </table>
+          </div>
+
+        </div>
       </PageDefaultLayout>
     </>
   );
