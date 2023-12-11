8a666df25df26a34bb6668a0ff1ad23ab295efa8
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-12-11 20:18:42 +0900

    Fix: 랜딩 페이지 수정

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -12,32 +12,63 @@ function LandingPage() {
   
   return (
     <>
-      <PageDefaultLayout className='landing_page'>
-        <header>
-          <h1>세종대학교 수강 신청 연습</h1>
-          <p>실제와 똑같은 경험으로 수강 신청을 연습해보세요.</p>
-        </header>
-        <div>
-
-          <section>
-            <div className='container'>
+      <PageDefaultLayout className='landing_page no_responsive'>
+        <section>
+          <div className='container figma'>
+            <div>
+              <h1>
+                세종대학교 <br/>
+                수강 신청 연습
+              </h1>
               <p>
-                안녕하세요! 세종대학교 수강 신청 연습 페이지에 오신 것을 환영합니다. 이 페이지는 수강 신청을 연습하고 각종 기능을 테스트할 수 있는 환경을 제공합니다.
+                실제 수강 신청 방식과 비슷하게 <br/>
+                수강 신청을 연습해보세요.
               </p>
+              <button onClick={() => navigate('/login')}>로그인 하기</button>
+            </div>
+            <div>
+              <img src='/Logo.png' alt=''/>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className='container'>
+            <div>
+              <img src='/vite.svg' alt=''/>
+            </div>
+            <div>
+              <h2>실제와 비슷한 환경</h2>
+              <p>
+                ALLCLL는 수강 신청을 연습하고 각종 기능을 테스트할 수 있는 환경을 제공합니다.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div className='container'>
+            <div>
+              <h2>다양한 과목</h2>
               <p>
                 여러 과목을 탐험하고 원하는 수업을 찾아보세요. 실제 수강 신청과 비슷한 프로세스를 경험하면서 미리 준비해보세요!
               </p>
               <button onClick={() => navigate('/login')}>로그인 하기</button>
             </div>
-          </section>
-        </div>
+            <div>
+              <img src='/vite.svg' alt=''/>
+
+            </div>
+          </div>
+        </section>
+
+        <footer>
+          <div className='container'>
+            <p>© 2023 ALLCLL. All rights reserved.</p>
+          </div>
+        </footer>
       </PageDefaultLayout>
 
-      <footer>
-        <div className='container'>
-          <p>© 2021 ALLCLL. All rights reserved.</p>
-        </div>
-      </footer>
     </>
   );
 }
