8135e4ee655811f14e06b6ad2eded1f1b2f8a82b
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-12-14 15:54:58 +0900

    Feat: 404 페이지 생성

--- src/pages/Page404.tsx
+++ src/pages/Page404.tsx
@@ -1,9 +1,18 @@
+import '@styles/Page404.scss';
+import {Link} from 'react-router-dom';
+
 function Page404() {
   document.title = 'ALLCLL | 페이지를 찾을 수 없습니다';
 
   return (
-    <div>
-      <h1>Page not found</h1>
+    <div className='page_404'>
+      <div>
+        <h1>404</h1>
+        <h2>Page Not Found</h2>
+        <p>Sorry, but the page you are looking for has not been found. Try checking the URL for errors, then hit the refresh button on your browser.</p>
+
+        <Link to='/'>Go to Home</Link>
+      </div>
     </div>
   );
 }
