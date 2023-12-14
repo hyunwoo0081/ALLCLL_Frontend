8a7dbe6478ac4ec57b911e1721a868cd06c03b98
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-12-14 21:01:01 +0900

    Fix: 로그인 제어 방식 변경

--- src/components/RoutingHelper.ts
+++ src/components/RoutingHelper.ts
@@ -2,8 +2,8 @@ import {useEffect} from 'react';
 import {useLocation, useNavigate} from 'react-router-dom';
 import useMobile from '../hooks/useMobile.ts';
 import useLogin from '../hooks/useLogin.ts';
-import RouteMap from "../constant/RouteMap.tsx";
-// import authControl from "../constant/AuthControl.ts";
+import RouteMap from '../constant/RouteMap.tsx';
+import authControl from '../constant/AuthControl.ts';
 
 function RoutingHelper() {
   const location = useLocation();
@@ -14,24 +14,25 @@ function RoutingHelper() {
   
   useEffect(() => {
     if (isMobile) {
-      window.location.href = '/';
+      if (location.pathname !== '/') {
+        authControl.logout(navigate, '/');
+      }
       return;
     }
 
     const route = RouteMap.find(v => v.path === location.pathname);
 
     if (!route) return;
-    if (route.auth.includes('GUEST') && isLogin) {
+    if (route.auth.length === 1 && route.auth[0] === 'GUEST' && isLogin) {
       navigate('/dashboard');
       return;
     }
-    if (route.auth.includes('USER') && !isLogin) {
-      // authControl.logout(navigate, '/');
+    else if (!route.auth.includes('GUEST') && !isLogin) {
       navigate('/');
       return;
     }
     
-  }, [location]);
+  }, [location, isMobile, isLogin]);
 
   return null;
 }
