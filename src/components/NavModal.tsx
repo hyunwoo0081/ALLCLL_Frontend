ef9fe8bb40ba2923e0eea59bde5a5a719a37968f
hyunwoo0081 <hyunwoo0081@gmail.com>
2023-12-15 15:58:47 +0900

    Feat: 비밀번호 변경 기능 숨김

--- src/components/NavModal.tsx
+++ src/components/NavModal.tsx
@@ -1,19 +1,19 @@
 import {useEffect, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
-import {IErrorTypes} from '../constant/CheckFetchError.ts';
+// import {IErrorTypes} from '../constant/CheckFetchError.ts';
 import AuthControl from '../constant/AuthControl.ts';
-import API from '../constant/API.ts';
+// import API from '../constant/API.ts';
 
 enum ToastView {
   CLOSED, TOAST_NORMAL, TOAST_ERROR
 }
-const ToastClass = ['closed', 'normal', 'error'];
+// const ToastClass = ['closed', 'normal', 'error'];
 
 function NavModal({isOpen, setIsOpen}: { isOpen: boolean, setIsOpen: (isOpen: boolean) => void }) {
   const navigate = useNavigate();
   const [overflow, setOverflow] = useState<string>('auto');
   const [toastType, setToastType] = useState<ToastView>(ToastView.CLOSED);
-  const [toastMessage, setToastMessage] = useState<string>('');
+  // const [toastMessage, setToastMessage] = useState<string>('');
 
   const token = AuthControl.getInfoFromToken();
 
@@ -53,40 +53,40 @@ function NavModal({isOpen, setIsOpen}: { isOpen: boolean, setIsOpen: (isOpen: bo
     setIsOpen(false);
   }
 
-  function changePassword() {
-    setToastType(ToastView.CLOSED);
+  // function changePassword() {
+  //   setToastType(ToastView.CLOSED);
+  //
+  //   if (!token) {
+  //     toastErrorMessage('로그인이 유효하지 않습니다');
+  //     return;
+  //   }
+  //
+  //   const email = token?.sub;
+  //   const Errors: IErrorTypes[] = [
+  //     {errorBody: 'Email address not found', errorMessage: '가입되지 않은 이메일 입니다'},
+  //     {errorBody: 'Invalid email format', errorMessage: '잘못된 메일 형식입니다'},
+  //   ];
+  //   API.fetch('/api/v2/auth/password/reset', 'POST', {email}, Errors, navigate)
+  //     .then(toastSendMail);
+  // }
 
-    if (!token) {
-      toastErrorMessage('로그인이 유효하지 않습니다');
-      return;
-    }
-
-    const email = token?.sub;
-    const Errors: IErrorTypes[] = [
-      {errorBody: 'Email address not found', errorMessage: '가입되지 않은 이메일 입니다'},
-      {errorBody: 'Invalid email format', errorMessage: '잘못된 메일 형식입니다'},
-    ];
-    API.fetch('/api/v2/auth/password/reset', 'POST', {email}, Errors, navigate)
-      .then(toastSendMail);
-  }
-
-  function toastSendMail() {
-    setToastType(ToastView.TOAST_NORMAL);
-    setToastMessage('가입하신 이메일로 변경 링크를 발송했습니다');
-  }
-
-  function toastErrorMessage(message: string) {
-    setToastType(ToastView.TOAST_ERROR);
-    setToastMessage(message);
-  }
+  // function toastSendMail() {
+  //   setToastType(ToastView.TOAST_NORMAL);
+  //   setToastMessage('가입하신 이메일로 변경 링크를 발송했습니다');
+  // }
+  //
+  // function toastErrorMessage(message: string) {
+  //   setToastType(ToastView.TOAST_ERROR);
+  //   setToastMessage(message);
+  // }
 
   return (
     <>
-      <div className='toast_box'>
-        <p className={ToastClass[toastType]}>
-          {toastMessage}
-        </p>
-      </div>
+      {/*<div className='toast_box'>*/}
+      {/*  <p className={ToastClass[toastType]}>*/}
+      {/*    {toastMessage}*/}
+      {/*  </p>*/}
+      {/*</div>*/}
 
       {isOpen && (
         <div className='dialog_background nav_dialog_background login_page'
@@ -95,7 +95,7 @@ function NavModal({isOpen, setIsOpen}: { isOpen: boolean, setIsOpen: (isOpen: bo
                onClick={e => e.stopPropagation()}>
             <ul>
               <li><span>{token.sub}</span></li>
-              <li><button onClick={changePassword}>비밀번호 변경</button></li>
+              {/*<li><button onClick={changePassword}>비밀번호 변경</button></li>*/}
               <li><button onClick={() => AuthControl.logout(navigate)}>로그아웃</button></li>
             </ul>
           </div>
