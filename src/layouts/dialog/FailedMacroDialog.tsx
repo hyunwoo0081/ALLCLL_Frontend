bd3414d3ee6b288da71727809b1f9936c33d457e
hyunwoo0081 <hyunwoo0081@gmail.com>
2024-01-16 16:24:02 +0900

    Fix: 매크로 실패 다이얼로그 내용 수정

--- src/layouts/dialog/FailedMacroDialog.tsx
+++ src/layouts/dialog/FailedMacroDialog.tsx
@@ -11,7 +11,6 @@ function FailedMacroDialog({isOpen, closeDialog}: IMacroDialog) {
     closeDialog();
   }
 
-  // Todo: 이거 작성
   return (
     <DialogTemplate isOpen={isOpen}>
       <div className='dialog_header'>
@@ -22,10 +21,8 @@ function FailedMacroDialog({isOpen, closeDialog}: IMacroDialog) {
       </div>
       <div className='dialog_body apply_dialog_body'>
         <div>
-          <img src='/CheckRing.svg' alt=''/>
-          <span>과목이 신청되었습니다. 수강신청내역을 재 조회 하시겠습니까?</span>
-          <span>취소를 선택하실 경우 [수강신청내역]이 갱신되지 않습니다</span>
-          <span>취소를 선택하실 경우 수강신청 최종 완료 후 반드시 [수강신청내역] 재조회를 눌러 신청내역을 확인하세요. [수강신청내역]에 조회된 과목만이 정상적으로 수강신청된 과목입니다</span>
+          <img src='/CloseRing.svg' alt=''/>
+          <span>입력된 코드가 일치하지 않습니다</span>
         </div>
       </div>
       <div className='dialog_footer'>
