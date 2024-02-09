920c8196cd641ec081f6c87adb2cd8a607905f78
hyunwoo0081 <hyunwoo0081@gmail.com>
2024-02-09 15:38:16 +0900

    Feat: Footer 크기 조절하는 기능 추가

--- src/layouts/PageDefaultLayout.tsx
+++ src/layouts/PageDefaultLayout.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Navigation from '../components/Navigation.tsx';
 import Footer from '../components/Footer.tsx';
 
 interface IPageDefaultLayout { className?: string, children?: React.ReactNode }
 
 function PageDefaultLayout({className='', children}: IPageDefaultLayout) {
+  const [navHeight, setNavHeight] = useState<number>(0);
+  const responsive = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const nav = document.querySelector('nav');
+
+    if (nav && nav.clientHeight && responsive.current && responsive.current.clientHeight) {
+      const computedStyle = window.getComputedStyle(responsive.current);
+
+      setNavHeight(
+        nav.clientHeight
+        + parseInt(computedStyle.paddingTop)
+        + parseInt(computedStyle.paddingBottom)
+        + parseInt(computedStyle.marginTop)
+        + parseInt(computedStyle.marginBottom)
+      );
+    }
+  }, []);
+
   return (
     <>
       <Navigation/>
-      <div className={`responsive ${className}`}>
+      <div className={`responsive ${className}`}
+           ref={responsive}
+           style={{
+             minHeight: `calc(100vh - ${navHeight}px)`,
+           }}>
         {children}
       </div>
       
