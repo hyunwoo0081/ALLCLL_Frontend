5872359b2e426057f881fd8c4b0b0608489bde46
hyunwoo0081 <hyunwoo0081@gmail.com>
2024-07-22 15:43:49 +0900

    Feat: 네비게이션 전환 css 추가

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import {useRef, useState} from 'react';
+import React, {useLayoutEffect, useRef, useState} from 'react';
 import {Link, useLocation, useNavigate} from 'react-router-dom';
 import useMobile from '../hooks/useMobile.ts';
 import useLogin from '../hooks/useLogin.ts';
@@ -24,17 +24,53 @@ const NavRoutes = [
 function Navigation() {
   const location = useLocation();
   const navigate = useNavigate();
-  const ref = useRef<HTMLButtonElement>(null)
-  
+  const ref = useRef<HTMLButtonElement>(null);
+
+  const [underlineStyle, setUnderlineStyle] = useState<React.CSSProperties>(
+    getUnderlineStyleDefault()
+  );
   const [modalOpened, setModalOpened] = useState<boolean>(false);
   
   const {isLogin} = useLogin();
   const {isMobile} = useMobile();
 
+  // Todo: 네비게이션 underlineStyle 객체 업데이트 될 때마다 바뀌는 버그 수정
+  //  리렌더링 방지 할 것
+  // console.log('Navigation rendered', underlineStyle);
+
   function openModal() {
     setModalOpened(true);
     ref.current?.blur();
   }
+
+  function getUnderlineStyle(target: HTMLAnchorElement) {
+    const width = target.offsetWidth - 20*2;
+    const transformX = target.offsetLeft - 64 + 20;
+
+    return {
+      width: width + 'px',
+      transform: `translate(${transformX}px, -2px)`
+    };
+  }
+
+  function getUnderlineStyleDefault() {
+    const target = document.querySelector('.selected') as HTMLAnchorElement;
+
+    if (target)
+      return getUnderlineStyle(target);
+    return {};
+  }
+
+  function setUnderlineTarget(target?: HTMLAnchorElement) {
+    const style = target ? getUnderlineStyle(target) : getUnderlineStyleDefault();
+
+    setUnderlineStyle(style);
+  }
+
+  useLayoutEffect(() => {
+    setUnderlineTarget();
+  }, []);
+
 
   return (
     <>
@@ -62,21 +98,23 @@ function Navigation() {
         </div>
         { isLogin && (
           <div className='navigation_shortcuts'>
-            <ul>
+            <ul onMouseLeave={() => setUnderlineTarget()}>
               {NavRoutes.map((route, index) => (
-                <li key={index}
-                    onClick={() => {
-                      if (location.pathname !== route.path)
-                        navigate(route.path);
-                    }}
-                    className={location.pathname === route.path ? 'selected' : ''}>
+                <li key={index}>
                   <Link to={route.path}
-                        className={location.pathname === route.path ? 'selected' : ''}>
+                        className={location.pathname === route.path ? 'selected' : ''}
+
+                        onMouseOver={e => setUnderlineTarget(e.target as HTMLAnchorElement)}
+                        onFocus={e => setUnderlineTarget(e.target as HTMLAnchorElement)}
+                        onBlur={() => setUnderlineTarget()}>
                     {route.name}
                   </Link>
                 </li>
               ))}
             </ul>
+            <div className='nav_underline'
+                  style={underlineStyle}
+                  aria-disabled/>
           </div>
         )}
       </nav>
