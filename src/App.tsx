538cd089e677e756d1c64d00a6ffe45f525ad8ed
hyunwoo0081 <hyunwoo0081@gmail.com>
2024-08-09 15:14:03 +0900

    Feat: 페이지 이동 시 스크롤 위로 올라가도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import RoutingHelper from './components/RoutingHelper.tsx';
 import RouteMap from './constant/RouteMap.tsx';
+import ScrollTop from "./components/ScrollTop.ts";
 
 function App() {
   return (
     <BrowserRouter>
       <RoutingHelper/>
+      <ScrollTop/>
       <Routes>
         {RouteMap.map((route, index) => (
           <Route key={index}
