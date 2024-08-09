34e4f346b9c526330849cb9dbfa501301ab52423
hyunwoo0081 <hyunwoo0081@gmail.com>
2024-08-09 14:57:55 +0900

    Feat: 회원가입 페이지 Footer 추가

--- src/pages/login/AgreeTermsPage.tsx
+++ src/pages/login/AgreeTermsPage.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import {useNavigate} from 'react-router-dom';
-import Navigation from '../../components/Navigation.tsx';
+import PageDefaultLayout from '../../layouts/PageDefaultLayout.tsx';
 import useLoginErrorBox from '../../hooks/useLoginErrorBox.tsx';
 import Terms from '../../layouts/Terms.tsx';
 import CheckStringType from '../../constant/CheckStringType.ts';
@@ -59,59 +59,55 @@ function AgreeTermsPage() {
   }
 
   return (
-    <>
-      <Navigation/>
-
-      <div className='responsive'>
-        <div className='terms_layout'>
-          <div className='terms_contents_layout'>
-            <Terms/>
-          </div>
-
-          <label htmlFor='terms_agree'>
-            <input type='checkbox'
-                   id='terms_agree'
-                   checked={agreed}
-                   onChange={e => setAgreed(e.target.checked)}/>
-            <span>위의 약관에 동의합니다</span>
-          </label>
+    <PageDefaultLayout>
+      <div className='terms_layout'>
+        <div className='terms_contents_layout'>
+          <Terms/>
         </div>
 
-        <div className='register_login_layout login_page'>
-          <div className='login_layout'>
-            <h1>ALLCLL 회원가입</h1>
-            <h2>
-              대양휴머니티칼리지에 로그인하여 인증합니다 <br/>
-              <b>ALLCLL</b>은 로그인 정보를 서버에 저장하지 않습니다
-            </h2>
+        <label htmlFor='terms_agree'>
+          <input type='checkbox'
+                 id='terms_agree'
+                 checked={agreed}
+                 onChange={e => setAgreed(e.target.checked)}/>
+          <span>위의 약관에 동의합니다</span>
+        </label>
+      </div>
 
-            {ErrorBox}
+      <div className='register_login_layout login_page'>
+        <div className='login_layout'>
+          <h1>ALLCLL 회원가입</h1>
+          <h2>
+            대양휴머니티칼리지에 로그인하여 인증합니다 <br/>
+            <b>ALLCLL</b>은 로그인 정보를 서버에 저장하지 않습니다
+          </h2>
 
-            <input type='text'
-                   placeholder='학번'
-                   autoComplete='username'
-                   ref={UserIdInputRef}
-                   disabled={fetching}
-                   value={userId}
-                   onChange={e => setUserId(e.target.value)}
-                   onKeyDown={onEnter}/>
-            <input type='password'
-                   placeholder='비밀번호'
-                   autoComplete='current-password'
-                   ref={PasswordInputRef}
-                   disabled={fetching}
-                   value={password}
-                   onChange={e => setPassword(e.target.value)}
-                   onKeyDown={onEnter}/>
+          {ErrorBox}
 
-            <button onClick={register} disabled={fetching || !agreed}>
-              {agreed ? '회원가입' : '약관에 동의해주세요'}
-            </button>
-            <button className='link' onClick={() => navigate('/login', {replace: true})}>이미 회원이신가요? 로그인하기</button>
-          </div>
+          <input type='text'
+                 placeholder='학번'
+                 autoComplete='username'
+                 ref={UserIdInputRef}
+                 disabled={fetching}
+                 value={userId}
+                 onChange={e => setUserId(e.target.value)}
+                 onKeyDown={onEnter}/>
+          <input type='password'
+                 placeholder='비밀번호'
+                 autoComplete='current-password'
+                 ref={PasswordInputRef}
+                 disabled={fetching}
+                 value={password}
+                 onChange={e => setPassword(e.target.value)}
+                 onKeyDown={onEnter}/>
+
+          <button onClick={register} disabled={fetching || !agreed}>
+            {agreed ? '회원가입' : '약관에 동의해주세요'}
+          </button>
+          <button className='link' onClick={() => navigate('/login', {replace: true})}>이미 회원이신가요? 로그인하기</button>
         </div>
       </div>
-    </>
+    </PageDefaultLayout>
   );
 }
 
